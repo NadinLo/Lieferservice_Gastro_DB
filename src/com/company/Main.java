87dd7eb2595ad6db6ad158fbaeb56e6221f8a54a
NadinLo <nadin.loewe@posteo.de>
2020-04-10 17:50:27 +0200

    method addIngredient

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //todo: Menü erstellen
+        //todo: Zutaten zufügen
+        //todo: Menügruppen verwalten
+        //todo: Liefergebiet verwalten
+        //todo: Lieferzonen verwalten
+    }
+    private static void addIngredient (String name, boolean vegetarian, double price) {
+
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
+            conn = DriverManager.getConnection(url);
+            String command = "INSERT INTO `zutaten`(`name`, `vegetarisch`, `preis`) VALUES " +
+                    "('" + name + "'," + vegetarian + "," + price +")";
+            Statement stmt = conn.createStatement();
+            int ok = stmt.executeUpdate(command);
+            if(ok == 1){
+                System.out.println("ingredient was successfully added");
+            } else {
+                System.out.println("something went wrong");
+            }
+
+        } catch (SQLException ex) {
+            throw new Error("Problem", ex);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
